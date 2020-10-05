c95c79337bba9eeab559e783661dcd999deeeb87
shuuuu10-01 <tanaka.shu642@mail.kyutech.jp>
2020-10-05 09:29:37 +0900

    start add

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
-    
-  </div>
+  <v-container fill-height>
+    <v-row>
+      <v-col>
+        <img alt="Vue logo" src="../assets/logo.png" class="image">
+        <br/>
+        <v-btn>スタート</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.image{
+  padding-top: 5px;
+  padding-bottom: 50px;
+}
+</style>
