2d7be5da2005aa95134ed4819a334574d1fd6136
shuuuu10-01 <tanaka.shu642@mail.kyutech.jp>
2020-10-05 20:21:05 +0900

    NaN 事件勃発

--- src/views/RTA.vue
+++ src/views/RTA.vue
@@ -3,8 +3,12 @@
     <v-row>
       <v-col>
         <h1>自炊RTA スタート！！</h1>
-        <img alt="logo" src="../assets/logo.png" class="image">
         <br/>
+        {{checkHours | zeroPadding}}：{{checkMinutes | zeroPadding}}：{{checkSeconds | zeroPadding}}：{{checkMiliSeconds | showMiliseconds}}
+        <br/>
+        <v-btn @click="start">スタート</v-btn>
+        <v-btn @click="stop">ストップ</v-btn>
+        <v-btn @click="reset">リセット</v-btn>
       </v-col>
     </v-row>
   </v-container>
@@ -12,5 +16,72 @@
 
 <script>
 export default {
+  date () {
+    return {
+      animationId: 0,
+      hours: 0,
+      minutes: 0,
+      second: 0,
+      milisecond: 0,
+      startTime: 0,
+      endTime: 0,
+      diffTime: 0,
+      flag: false
+    }
+  },
+  filters: {
+    zeroPadding (value) {
+      return value.toString().padStart(2, 0)
+    },
+    showMiliseconds (value) {
+      return value.toString().padStart(3, 0)
+    }
+  },
+  methods: {
+    setStartTime (time) {
+      this.startTime = performance.now() - time
+    },
+    start () {
+      if (this.flag) {
+        return false
+      }
+      // eslint-disable-next-line camelcase
+      const vm_data = this
+      this.flag = true
+      this.setStartTime(vm_data.diffTime);
+      (function progress () {
+        vm_data.endTime = performance.now()
+        vm_data.diffTime = (vm_data.endTime - vm_data.startTime)
+        vm_data.second = Math.floor(vm_data.diffTime / 1000)
+        vm_data.milisecond = Math.floor(vm_data.diffTime % 1000)
+        vm_data.animationId = window.requestAnimationFrame(progress)
+      }())
+    },
+    stop () {
+      this.flag = false
+      window.cancelAnimationFrame(this.animationId)
+    },
+    reset () {
+      if (this.flag) {
+        return false
+      }
+      this.startTime = this.diffTime = 0
+    }
+  },
+  computed: {
+    checkHours () {
+      console.log(this.second)
+      return Math.floor(this.diffTime / 1000 / 60 / 60)
+    },
+    checkMinutes () {
+      return Math.floor(this.diffTime / 1000 / 60) % 60
+    },
+    checkSeconds () {
+      return Math.floor(this.diffTime / 1000) % 60
+    },
+    checkMiliSeconds () {
+      return Math.floor(this.diffTime % 1000)
+    }
+  }
 }
 </script>
